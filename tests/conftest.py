4ac51b62149fb0b5b70ff8d76718fc27899527fc
kailaniwoodard <woodardk@allegheny.edu>
2021-04-30 16:40:10 -0400

    Created pytest_addoption

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
 pytest_plugins = ["pytester"]
+
+def pytest_addoption(parser):
+    testplan = parser.getgroup('testplan')
+    testplan.addoption("--testplan",
+        action="store",
+        default=None,
+        help="Generate a .csv file containing the test's metadata and exit without running tests."
+    )
